f263bfef536db743d8b9409df3457b1365958c7c
Brayan Ccari Lindo <brayancclindo@gmail.com>
2023-09-25 23:37:08 -0500

    brayanCcari

--- src/components/portfolio-context.tsx
+++ src/components/portfolio-context.tsx
@@ -4,7 +4,7 @@ export type Project = {
   title: string;
   slug: string;
   slogan: string;
-  description: string;
+  description: string[];
   languages: string[];
   cover: string;
   github: string;
@@ -113,18 +113,20 @@ export function PortfolioProvider({ children }: { children: ReactNode }) {
       slug: "weather-app",
       slogan:
         "A weather application that provides real-time information on weather conditions based on the user's location and time.",
-      description:
-        "A weather application that provides real-time information on weather conditions based on the user's location and time. Built in Vanilla JavaScript, CSS, HTML, ViteJs.",
+      description: [
+        "Weather App is an intuitive and comprehensive weather app that keeps you always one step ahead when it comes to weather conditions. From the convenience of your smartphone or tablet,",
+        " Weather App gives you instant access to a wide range of weather information, providing an exceptional user experience.",
+      ],
+
       languages: ["Vanilla Javascript", "HTML", "CSS"],
       cover: "../../images/weather-app.jpg",
-      image: "../../images/iBook/iBookPortrait.jpg",
+      image: "../../images/weather-Photo.jpg",
 
       photos: [
-        "../../images/iBook/iBookPhoto2.jpg",
-        "../../images/iBook/iBookPhoto3.jpg",
-
-        "../../images/iBook/iBookPhoto4.jpg",
-        "../../images/iBook/iBookPhoto5.jpg",
+        "../../images/weather-app/weatherApp-Photo1.jpg",
+        "../../images/weather-app/weatherApp-Photo2.jpg",
+        "../../images/weather-app/weatherApp-Photo3.jpg",
+        "../../images/weather-app/weatherApp-Photo4.jpg",
       ],
       github: "https://github.com/BrayanCcLindo/weather-app",
       web: "https://brayancclindo.github.io/weather-app/",
@@ -134,11 +136,13 @@ export function PortfolioProvider({ children }: { children: ReactNode }) {
       slug: "ibookshelf-app",
       slogan:
         "Apple Books es el lugar ideal para acceder a tus libros favoritos y los que estás por descubrir",
-      description:
-        "ibookshelf is the ideal place to access your favorite and undiscovered books. Explore the Store tab and find the perfect book for you. Keep track of your favorite books and what you want to read, and set your own Reading Goals, all in one app.",
-      languages: ["ReactJs", "TypeScript", "Tailwind CSS"],
+      description: [
+        "ibookshelf is the ideal place to access your favorite and undiscovered books. Explore the Store tab and find the perfect book for you. Keep track of your favorite books and what you want to read, and set your own Reading Goals, all in one app",
+        "This application has multiple robust and versatile filtering features that allow you to browse our extensive collection of books in an efficient and personalized way. With our filter options, you will easily find your next favorite read.",
+      ],
+      languages: ["HMTL", "Tailwind CSS", "ReactJs", "TypeScript", "Radix UI"],
       cover: "../../images/test.jpg",
-      image: "../../images/iBook/iBookPortrait.jpg",
+      image: "../../images/iBookShelf-photo.jpg",
 
       photos: [
         "../../images/iBook/iBookPhoto2.jpg",
@@ -155,11 +159,13 @@ export function PortfolioProvider({ children }: { children: ReactNode }) {
       slug: "game-store",
       slogan:
         "Web app providing video game info, platform availability, and filtering via RESTful APIs.",
-      description:
-        "ibookshelf is the ideal place to access your favorite and undiscovered books. Explore the Store tab and find the perfect book for you. Keep track of your favorite books and what you want to read, and set your own Reading Goals, all in one app.",
+      description: [
+        "We understand that every gamer has unique tastes and preferences. That's why we have developed a comprehensive and versatile filtering feature that allows you to efficiently browse and find your ideal games. With our filtering options, customize your gaming experience according to your desires.",
+        "Filter video games by genre, whether action, adventure, role-playing, strategy, sports, simulation, and many more. Find games that suit your style and preferences.",
+      ],
       languages: ["Vanilla Javascript", "HTML", "CSS"],
       cover: "../../images/game-app/gameApp.jpg",
-      image: "../../images/game-app/gameApp-portrait.jpg",
+      image: "../../images/gameStore-photo.jpg",
       photos: [
         "../../images/game-app/gameAppPhoto1.jpg",
         "../../images/game-app/gameAppPhoto2.jpg",
@@ -174,9 +180,11 @@ export function PortfolioProvider({ children }: { children: ReactNode }) {
       title: "Pokedex",
       slug: "pokedex",
       slogan:
-        "¡Explora el emocionante mundo Pokémon y conoce más de tu Pokémon favorito!",
-      description:
-        "ibookshelf is the ideal place to access your favorite and undiscovered books. Explore the Store tab and find the perfect book for you. Keep track of your favorite books and what you want to read, and set your own Reading Goals, all in one app.",
+        "Explore the exciting world of Pokémon and learn more about your favorite Pokémon!",
+      description: [
+        "Every Pokémon Trainer has their special companion, their favorite Pokémon that has accompanied them on countless adventures. That's why we've created an exceptional tool that allows you to find your favorite Pokémon and explore all its characteristics and evolutions in detail.",
+        "That's why we've incorporated a special feature that lets you find your favorite Pokémon and listen to all of its characteristics in detail. Whether you prefer Pikachu, Charizard, Eevee, or any other Pokémon, you'll be able to find your favorite Pokémon.",
+      ],
       languages: ["Vanilla Javascript", "HTML", "CSS"],
       cover: "../../images/pokedex/pokedex.jpg",
       image: "../../images/pokedex/pokedexPortrait.jpg",
@@ -193,12 +201,15 @@ export function PortfolioProvider({ children }: { children: ReactNode }) {
       title: "GitHub Clone",
       slug: "github-clone",
       slogan:
-        "Un clon de GitHub que te brinda la capacidad de explorar perfiles y repositorios.",
-      description:
-        "ibookshelf is the ideal place to access your favorite and undiscovered books. Explore the Store tab and find the perfect book for you. Keep track of your favorite books and what you want to read, and set your own Reading Goals, all in one app.",
-      languages: ["ReactJs", "TypeScript", "Tailwind CSS"],
+        "A GitHub clone that gives you the ability to browse profiles and repositories",
+      description: [
+        "At our development platform, we know that every developer has unique needs when managing and collaborating on source code projects. That's why we've created a comprehensive and versatile filtering feature that allows you to navigate and find your repositories and projects efficiently.",
+        "With our filtering options, customize your experience, quickly find any repository or user using our powerful search bar.",
+      ],
+      languages: ["HTML", "Styled-Components", "React.Js", "Radix UI"],
       cover: "../../images/github/github-clone.jpg",
-      image: "../../images/github/githubPortrait.jpg",
+      image: "../../images/githubClone-photo.jpg",
+
       photos: [
         "../../images/github/githubPhoto1.jpg",
         "../../images/github/githubPhoto2.jpg",
--- src/pages/ProjectName.tsx
+++ src/pages/ProjectName.tsx
@@ -19,10 +19,11 @@ function Project() {
         <h1 className="text-7xl font-semibold mb-8">{oficialProyect.title}</h1>
       </Layout>
       <img
-        className="h-[500px]  w-full object-cover object-left-top"
+        className="h-[500px]  w-full object-cover object-center"
         src={oficialProyect.image}
         alt=""
       />
+
       <Layout>
         <div className="grid grid-cols-[1fr,2fr] gap-8 p-8 bg-white rounded-xl mt-16">
           <div className="bg-[#FBFBFC] rounded-xl p-8 flex flex-col gap-8">
@@ -33,18 +34,18 @@ function Project() {
               </p>
             ))}
           </div>
-          <div>
-            <h2 className="text-2xl text-black font-semibold mb-8">
-              Description
-            </h2>
-            <p className="text-xl"> {oficialProyect.description}</p>
+          <div className="flex flex-col gap-8">
+            <h2 className="text-2xl text-black font-semibold">Description</h2>
+            {oficialProyect.description.map((description) => (
+              <p className="text-xl"> {description}</p>
+            ))}
           </div>
         </div>
         <div className="grid grid-cols-[1fr,1fr] gap-16 rounded-xl mt-16">
           {oficialProyect.photos.map((photo, index) => (
             <img
               key={index}
-              className="rounded-3xl object-right-top object-cover h-[500px]"
+              className="rounded-3xl w-full object-center object-cover h-[500px]"
               src={photo}
               alt=""
             />
